90d5febcdee0358b960d4a529fa4c9aace8d21d8
heshegto <heshkin@gmail.com>
2023-11-22 17:56:28 +0400

    Second commit. Everything works, correctly parsing all items in store. Need to add brand name for all products

--- main.py
+++ main.py
@@ -1,35 +1,69 @@
 import requests
 import lxml
 from bs4 import BeautifulSoup as BS
+from time import sleep
+import json
 
 base_url = 'https://online.metro-cc.ru'
+headers = {"User-Agent":
+               "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 OPR/104.0.0.0"}
 
-url = 'https://online.metro-cc.ru/category/bezalkogolnye-napitki/napitki-105003?in_stock=1&page=1'
-response = requests.get(url)
-
+# Подсчет количества страниц
+url = 'https://online.metro-cc.ru/category/bezalkogolnye-napitki/napitki-105003?in_stock=1'
+response = requests.get(url, headers=headers)
 soup = BS(response.text, "lxml")
+number_of_pages = soup.find_all('a', class_="v-pagination__item catalog-paginate__item")[-1].text
 
-data = soup.find('div',
-                 class_='catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating with-prices-drop')
-pk_id = data.get('data-sku')
-name = data.find('span', class_="product-card-name__text").text.replace('\n', '').lstrip().rstrip()
+base = {}
 
-promo_price_field = data.find('div', class_='product-unit-prices__actual-wrapper')
-promo_price = promo_price_field.find('span', class_='product-price__sum-rubles').text
-if promo_price_field.find('span', class_='product-price__sum-penny') is not None:
-    promo_price += promo_price_field.find('span', class_='product-price__sum-penny').text
+for count in range(1, int(number_of_pages)+1):
+    sleep(3)
 
-price_field = data.find('div', class_="product-unit-prices__old-wrapper")
-price = price_field.find('span', class_='product-price__sum-rubles')
-if price_field.find('span', class_='product-price__sum-penny') is not None and price is not None:
-    price = price.text + price_field.find('span', class_='product-price__sum-penny').text
-elif price is not None:
-    price = price.text
-else:
-    price, promo_price = promo_price, price
-href = base_url + data.find('a', class_="product-card-photo__link reset-link").get('href')
-response = requests.get(href)
-soup = BS(response.text, 'lxml')
-brand = soup.find('span', class_="product-attributes__list-item-value").text.replace('\n', '').lstrip().rstrip()
+    url = f'https://online.metro-cc.ru/category/bezalkogolnye-napitki/napitki-105003?in_stock=1&page={count}'
+    response = requests.get(url, headers=headers)
+    soup = BS(response.text, "lxml")
+    class_='catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating with-prices-drop'
+    data = soup.find_all('div', class_=class_)
 
-print(pk_id, name,href, price, promo_price, brand, sep='\n')
+    for item in data:
+        pk = item.get('data-sku')
+
+        base_item = {
+            'id': pk.strip(),
+            'name': item.find('span', class_="product-card-name__text").text.replace('\n', '').lstrip().rstrip()
+        }
+
+        promo_price_field = item.find('div', class_='product-unit-prices__actual-wrapper')
+        promo_price = promo_price_field.find('span', class_='product-price__sum-rubles').text
+        if promo_price_field.find('span', class_='product-price__sum-penny') is not None:
+            promo_price += promo_price_field.find('span', class_='product-price__sum-penny').text
+
+        price_field = item.find('div', class_="product-unit-prices__old-wrapper")
+        price = price_field.find('span', class_='product-price__sum-rubles')
+        if price_field.find('span', class_='product-price__sum-penny') is not None and price is not None:
+            price = price.text + price_field.find('span', class_='product-price__sum-penny').text
+        elif price is not None:
+            price = price.text
+        else:
+            price, promo_price = promo_price, 'No promo'
+
+        href = base_url + item.find('a', class_="product-card-photo__link reset-link").get('href')
+
+        base_item['url'] = href.strip()
+        base_item['price'] = price.strip().replace(u"\u00A0", "")
+        base_item['promo_price'] = promo_price.strip().replace(u"\u00A0", "")
+
+        base[pk] = base_item
+
+print(len(base))
+
+with open('result.json', 'w', encoding='utf-8') as output:
+    json.dump(base, output, ensure_ascii=False)
+
+"""
+    response = requests.get(href)
+    soup = BS(response.text, 'lxml')
+    brand = soup.find('span', class_="product-attributes__list-item-value").text.replace('\n', '').lstrip().rstrip()
+
+    print(pk_id, name,href, price, promo_price, brand, sep='\n')
+"""
