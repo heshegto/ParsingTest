2953672e5ce9711bb0489c580bc19f334176fc75
heshegto <heshkin@gmail.com>
2023-11-22 18:30:07 +0400

    Final commit. Everything works correctly. Task done

--- main.py
+++ main.py
@@ -14,39 +14,50 @@ response = requests.get(url, headers=headers)
 soup = BS(response.text, "lxml")
 number_of_pages = soup.find_all('a', class_="v-pagination__item catalog-paginate__item")[-1].text
 
-base = {}
+base = {}  # Словарь в который будут записаны данные
 
 for count in range(1, int(number_of_pages)+1):
     sleep(3)
 
+    # Считывание элементов на текущей странице
     url = f'https://online.metro-cc.ru/category/bezalkogolnye-napitki/napitki-105003?in_stock=1&page={count}'
     response = requests.get(url, headers=headers)
     soup = BS(response.text, "lxml")
-    class_='catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating with-prices-drop'
+    class_ = 'catalog-2-level-product-card product-card subcategory-or-type__products-item with-rating with-prices-drop'
     data = soup.find_all('div', class_=class_)
 
     for item in data:
+        # id
         pk = item.get('data-sku')
 
         base_item = {
             'id': pk.strip(),
+            # Наименование товара
             'name': item.find('span', class_="product-card-name__text").text.replace('\n', '').lstrip().rstrip()
         }
 
+        # Промо-цена
         promo_price_field = item.find('div', class_='product-unit-prices__actual-wrapper')
         promo_price = promo_price_field.find('span', class_='product-price__sum-rubles').text
+        # Проверяем есть ли копейки в цене
         if promo_price_field.find('span', class_='product-price__sum-penny') is not None:
             promo_price += promo_price_field.find('span', class_='product-price__sum-penny').text
 
+        # Регулярная цена
         price_field = item.find('div', class_="product-unit-prices__old-wrapper")
         price = price_field.find('span', class_='product-price__sum-rubles')
+        # Здесь также проверяется есть ли копейки в цене, а также есть ли промо-акция на товар
+        # В поле которое считывается выше, в разделе Промо-цена, попадает либо Промо-цена (если она существует)
+        # либо Регулярная цена (если нет Промо-цены), поле, которое рассматривается в этом разделе
+        # заполняется Регулярной ценой (если Промо-цена существует) или остается пустой (если Промо-цены нет)
         if price_field.find('span', class_='product-price__sum-penny') is not None and price is not None:
             price = price.text + price_field.find('span', class_='product-price__sum-penny').text
         elif price is not None:
             price = price.text
-        else:
+        else:  # Случай без промо-акции
             price, promo_price = promo_price, 'No promo'
 
+        # Ссылка на товар
         href = base_url + item.find('a', class_="product-card-photo__link reset-link").get('href')
 
         base_item['url'] = href.strip()
@@ -57,13 +68,15 @@ for count in range(1, int(number_of_pages)+1):
 
 print(len(base))
 
+for item in base:
+    # Добавляем к записям название бренда
+    sleep(3)
+    href = base[item]['url']
+    response = requests.get(href, headers=headers)
+    soup = BS(response.text, 'lxml')
+    base[item]['brand'] = soup.find('span',
+                                    class_="product-attributes__list-item-value"
+                                    ).text.replace('\n', '').lstrip().rstrip()
+
 with open('result.json', 'w', encoding='utf-8') as output:
     json.dump(base, output, ensure_ascii=False)
-
-"""
-    response = requests.get(href)
-    soup = BS(response.text, 'lxml')
-    brand = soup.find('span', class_="product-attributes__list-item-value").text.replace('\n', '').lstrip().rstrip()
-
-    print(pk_id, name,href, price, promo_price, brand, sep='\n')
-"""
